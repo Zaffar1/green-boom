48195e252b81e16fdb867abd801d42826cf89b94
bilal-vrcreators <bilal.tunio@vrcreators.co>
2023-12-29 20:17:38 +0500

    perfect sale media

--- app/Http/Controllers/PerfectSaleController.php
+++ app/Http/Controllers/PerfectSaleController.php
@@ -107,6 +107,23 @@ class PerfectSaleController extends Controller
             if (!$perfect_sale)
                 return response()->json(["message" => "Invalid perfect sale"]);
             else
+                $perfectSaleMedia = PerfectSaleMedia::wherePerfectSaleId($id)->orderBy('id', 'DESC')->with('perfectSales')->get();
+            return response()->json(["data" => $perfectSaleMedia]);
+        } catch (\Throwable $th) {
+            return response()->json(["error" => $th->getMessage()], 400);
+        }
+    }
+
+    public function customerPerfectSaleMedia(Request $request, $id)
+    {
+        // $request->validate([
+        //     'perfect_sale_id' => 'required',
+        // ]);
+        try {
+            $perfect_sale = PerfectSale::find($id);
+            if (!$perfect_sale)
+                return response()->json(["message" => "Invalid perfect sale"]);
+            else
                 $perfectSaleMedia = PerfectSaleMedia::wherePerfectSaleId($id)->orderBy('id', 'DESC')->whereStatus('Active')->with('perfectSales')->get();
             return response()->json(["data" => $perfectSaleMedia]);
         } catch (\Throwable $th) {
