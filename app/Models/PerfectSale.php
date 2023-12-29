a9c20b7c970037f226d1f44e11f66cde9590e213
bilal-vrcreators <bilal.tunio@vrcreators.co>
2023-12-29 20:34:39 +0500

    perfect sale media

--- app/Models/PerfectSale.php
+++ app/Models/PerfectSale.php
@@ -15,6 +15,6 @@ class PerfectSale extends Model
 
     public function perfectSaleMedia()
     {
-        return $this->hasMany(PerfectSaleMedia::class);
+        return $this->hasMany(PerfectSaleMedia::class, 'id', 'perfect_sale_id');
     }
 }
