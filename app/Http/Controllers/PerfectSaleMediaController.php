ddfee055c1e8cb1d3c1228e1792cf2f3974aebad
bilal-vrcreators <bilal.tunio@vrcreators.co>
2024-01-01 21:05:32 +0500

    perfect sale pitch

--- app/Http/Controllers/PerfectSaleMediaController.php
+++ app/Http/Controllers/PerfectSaleMediaController.php
@@ -14,7 +14,7 @@ class PerfectSaleMediaController extends Controller
         $validate = $request->validate([
             'perfect_sale_id' => 'required',
             'title' => 'required',
-            // 'file' => 'required|mimes:pdf,mp4,mov,avi,doc,docx,ppt,pptx,xls,xlsx',
+            'file' => 'required|mimes:pdf,mp4,mov,avi,doc,docx,ppt,pptx,xls,xlsx',
         ]);
 
         try {
@@ -26,35 +26,33 @@ class PerfectSaleMediaController extends Controller
                 $validate['status'] = 'Active';
             }
 
-            if ($request->file('file')) {
-                $file = $request->file('file');
-                $new_name = time() . '.' . $file->extension();
-                $file->move(public_path('storage/perfectSaleMedia'), $new_name);
-                $validate['file'] = "storage/perfectSaleMedia/$new_name";
+            $file = $request->file('file');
+            $new_name = time() . '.' . $file->extension();
+            $file->move(public_path('storage/perfectSaleMedia'), $new_name);
+            $validate['file'] = "storage/perfectSaleMedia/$new_name";
 
-                $file_type = $file->getClientOriginalExtension();
+            $file_type = $file->getClientOriginalExtension();
 
+            $video_extension = ['mp4', 'avi', 'mov', 'wmv'];
+            $pdf_extension = ['pdf'];
+            $word_extension = ['doc', 'docx'];
+            $ppt_extension = ['ppt', 'pptx'];
+            $excel_extension = ['xls', 'xlsx'];
 
-                $video_extension = ['mp4', 'avi', 'mov', 'wmv'];
-                $pdf_extension = ['pdf'];
-                $word_extension = ['doc', 'docx'];
-                $ppt_extension = ['ppt', 'pptx'];
-                $excel_extension = ['xls', 'xlsx'];
-
-                if (in_array(strtolower($file_type), $video_extension)) {
-                    $validate['file_type'] = 'video';
-                } elseif (in_array(strtolower($file_type), $pdf_extension)) {
-                    $validate['file_type'] = 'pdf';
-                } elseif (in_array(strtolower($file_type), $word_extension)) {
-                    $validate['file_type'] = 'word';
-                } elseif (in_array(strtolower($file_type), $ppt_extension)) {
-                    $validate['file_type'] = 'ppt';
-                } elseif (in_array(strtolower($file_type), $excel_extension)) {
-                    $validate['file_type'] = 'excel';
-                } else {
-                    $validate['file_type'] = 'other';
-                }
+            if (in_array(strtolower($file_type), $video_extension)) {
+                $validate['file_type'] = 'video';
+            } elseif (in_array(strtolower($file_type), $pdf_extension)) {
+                $validate['file_type'] = 'pdf';
+            } elseif (in_array(strtolower($file_type), $word_extension)) {
+                $validate['file_type'] = 'word';
+            } elseif (in_array(strtolower($file_type), $ppt_extension)) {
+                $validate['file_type'] = 'ppt';
+            } elseif (in_array(strtolower($file_type), $excel_extension)) {
+                $validate['file_type'] = 'excel';
+            } else {
+                $validate['file_type'] = 'other';
             }
+
             PerfectSaleMedia::create($validate);
             return response()->json(["message" => "Perfect sale data successfully added"], 200);
         } catch (\Throwable $th) {
