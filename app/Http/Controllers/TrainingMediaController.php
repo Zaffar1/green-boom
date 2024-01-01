461cd6c140d954332029bd63dc01a3a974b3db5a
bilal-vrcreators <bilal.tunio@vrcreators.co>
2024-01-01 18:52:50 +0500

    perfect sale pitch

--- app/Http/Controllers/TrainingMediaController.php
+++ app/Http/Controllers/TrainingMediaController.php
@@ -110,6 +110,7 @@ class TrainingMediaController extends Controller
                     return response()->json(["message" => "Invalid perfect sale"]);
                 else
                     $trainingMedia = PerfectSaleMedia::wherePerfectSaleId($training->id)->orderBy('id', 'DESC')->with('scriptMedia')->get();
+                return response()->json(["data" => $trainingMedia]);
             } else
                 $training = Training::find($request->id);
             if (!$training)
