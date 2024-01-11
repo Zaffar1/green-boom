c013b231df65be8082afc6b85b8d1ffbcf59d994
bilal-vrcreators <bilal.tunio@vrcreators.co>
2024-01-11 19:02:07 +0500

    Product Data Work

--- app/Http/Controllers/ProductAllDataController.php
+++ app/Http/Controllers/ProductAllDataController.php
@@ -25,12 +25,12 @@ class ProductAllDataController extends Controller
             $sizePickerArray = [];
 
             // Small size
-            $product_small = ProductDataSize::whereProductId($id)->whereSize('small')->get();
+            $product_small = ProductDataSize::whereProductId($id)->whereSize('small')->first();
             if ($product_small) {
                 $product_data_small['size'] = $product_small;
 
                 // Small size dimension
-                $product_small_dimension = ProductDataDimension::whereProductDataSizeId($product_small[0]->id)->first();
+                $product_small_dimension = ProductDataDimension::whereProductDataSizeId($product_small->id)->first();
                 if ($product_small_dimension) {
                     $product_data_small['dimension'] = $product_small_dimension;
                 } else {
@@ -38,7 +38,7 @@ class ProductAllDataController extends Controller
                 }
 
                 // Small size title_sku
-                $product_data_title_sku = ProductDataTitle::whereProductDataSizeId($product_small[0]->id)->first();
+                $product_data_title_sku = ProductDataTitle::whereProductDataSizeId($product_small->id)->first();
                 if ($product_data_title_sku) {
                     $product_data_small['title'] = $product_data_title_sku;
                 } else {
@@ -46,20 +46,18 @@ class ProductAllDataController extends Controller
                 }
 
                 // Add small size to sizePickerArray
-                $sizePickerArray[] = ["id" => "small", "title" => "small"];
+                $sizePickerArray[] = (object)["id" => "small", "title" => "small"];
             } else {
                 $product_data_small = [];
             }
 
             // Medium size
-            $product_medium = ProductDataSize::whereProductId($id)->whereSize('medium')->get();
+            $product_medium = ProductDataSize::whereProductId($id)->whereSize('medium')->first();
             if ($product_medium) {
-                // $product_data_medium['product_medium'] = $product_medium;
-                $product_data_medium = $product_medium;
-
+                $product_data_medium['size'] = $product_medium;
 
                 // Medium size dimension
-                $product_medium_dimension = ProductDataDimension::whereProductDataSizeId($product_medium[0]->id)->first();
+                $product_medium_dimension = ProductDataDimension::whereProductDataSizeId($product_medium->id)->first();
                 if ($product_medium_dimension) {
                     $product_data_medium['dimension'] = $product_medium_dimension;
                 } else {
@@ -67,7 +65,7 @@ class ProductAllDataController extends Controller
                 }
 
                 // Medium size title_sku
-                $product_data_title_sku = ProductDataTitle::whereProductDataSizeId($product_medium[0]->id)->first();
+                $product_data_title_sku = ProductDataTitle::whereProductDataSizeId($product_medium->id)->first();
                 if ($product_data_title_sku) {
                     $product_data_medium['title'] = $product_data_title_sku;
                 } else {
@@ -75,18 +73,18 @@ class ProductAllDataController extends Controller
                 }
 
                 // Add medium size to sizePickerArray
-                $sizePickerArray[] = ["id" => "medium", "title" => "medium"];
+                $sizePickerArray[] = (object)["id" => "medium", "title" => "medium"];
             } else {
                 $product_data_medium = [];
             }
 
             // Large size
-            $product_large = ProductDataSize::whereProductId($id)->whereSize('large')->get();
+            $product_large = ProductDataSize::whereProductId($id)->whereSize('large')->first();
             if ($product_large) {
-                $product_data_large['large'] = $product_large;
+                $product_data_large['size'] = $product_large;
 
                 // Large size dimension
-                $product_large_dimension = ProductDataDimension::whereProductDataSizeId($product_large[0]->id)->first();
+                $product_large_dimension = ProductDataDimension::whereProductDataSizeId($product_large->id)->first();
                 if ($product_large_dimension) {
                     $product_data_large['dimension'] = $product_large_dimension;
                 } else {
@@ -94,7 +92,7 @@ class ProductAllDataController extends Controller
                 }
 
                 // Large size title_sku
-                $product_data_title_sku = ProductDataTitle::whereProductDataSizeId($product_large[0]->id)->first();
+                $product_data_title_sku = ProductDataTitle::whereProductDataSizeId($product_large->id)->first();
                 if ($product_data_title_sku) {
                     $product_data_large['title'] = $product_data_title_sku;
                 } else {
@@ -102,7 +100,7 @@ class ProductAllDataController extends Controller
                 }
 
                 // Add large size to sizePickerArray
-                $sizePickerArray[] = ["id" => "large", "title" => "large"];
+                $sizePickerArray[] = (object)["id" => "large", "title" => "large"];
             } else {
                 $product_data_large = [];
             }
