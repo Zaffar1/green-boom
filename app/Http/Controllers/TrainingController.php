160731a82f731ab813691941edbe7498d761d3c3
bilal-vrcreators <bilal.tunio@vrcreators.co>
2024-01-23 21:08:22 +0500

    Sned mail work

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -37,7 +37,7 @@ class TrainingController extends Controller
                     $data = MsdSheet::whereStatus('Active')->orderBy('id', 'DESC')->get();
                     break;
 
-                case 'videos':
+                case 'Video':
                     $data = Video::whereStatus('Active')->orderBy('id', 'DESC')->get();
                     break;
 
