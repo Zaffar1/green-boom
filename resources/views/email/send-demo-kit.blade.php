82647200c6b85f3aab3a718512661a9be88e439f
bilal-vrcreators <bilal.tunio@vrcreators.co>
2024-01-23 18:17:52 +0500

    Sned mail work

--- resources/views/email/send-demo-kit.blade.php
+++ resources/views/email/send-demo-kit.blade.php
@@ -4,13 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order a kit mail</title>
+    <title>Order Kit Mail</title>
 </head>
 
 <body>
     <p>Dear Admin,</p>
-    <p>You get data from this data from customer</p>
-    {{ $data }}
+    <p>You have received the following data from the customer:</p>
+
+    <pre>{{ print_r($data, true) }}</pre>
 
     <p>Thank you!</p>
 </body>
