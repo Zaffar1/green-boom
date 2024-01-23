cee06a5898acd858022d83000e9fe5c403ef5f59
bilal-vrcreators <bilal.tunio@vrcreators.co>
2024-01-23 18:50:37 +0500

    Sned mail work

--- app/Http/Controllers/OrderKitFormController.php
+++ app/Http/Controllers/OrderKitFormController.php
@@ -18,7 +18,7 @@ class OrderKitFormController extends Controller
                 "last_name" => $request->last_name,
                 "email" => $request->email,
                 "phone" => $request->phone,
-                "company_name" => $request->company_name,
+                "company" => $request->company,
                 "country" => $request->country,
                 "state" => $request->state,
                 "zip_code" => $request->zip_code,
